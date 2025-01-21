53d3bf1d9e7761f5c7c5624785fde0574a5193be
alvyn <alvyn.richard@hotmail.fr>
2025-01-21 14:08:43 +0100

    By Alvyn Header réalisé quelques soucis de  responsive mais se sera corriger plus tard

---
+++ pages/components/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ICO</title>
+    <link rel="icon" href="../ressources/image/ICO_Logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../ressources/css/header.css">
+    <link rel="stylesheet" href="../ressources/css/all.css">
+</head>
+<body>
+    <div class="container-navbar">
+        <div class="container-logo">
+            <a href="#"><img src="../ressources/image/ICO_Logo.png" alt="logo-ico"></a>
+        </div>
+        <div class="container-navigation">
+            <ul>
+                <li><a href="dashboard">Dashboard</a></li>
+                <li><a href="avis.php">Avis</a></li>
+                <li><a href="faq.php">FAQ</a></li>
+                <li><a href="contact.php">contact</a></li>
+                <li><a href="acheter.php">Acheter</a></li>
+            </ul>
+            <div class="container-login-user">
+                <img src="../ressources/image/utilisateur.png" alt="logo-utilisateur">
+            </div>
+        </div>
+    </div>
+
---
+++ pages/components/headerHero.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ICO</title>
+    <link rel="icon" href="../ressources/image/ICO_Logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../ressources/css/navbar.css">
+    <link rel="stylesheet" href="../ressources/css/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="container-main-header">
+    <div class="container-navbar">
+        <div class="container-logo">
+            <a href="#"><img src="../ressources/image/ICO_Logo.png" alt="logo-ico"></a>
+        </div>
+        <div class="container-navigation">
+            <ul>
+                <li><a href="dashboard">Dashboard</a></li>
+                <li><a href="avis.php">Avis</a></li>
+                <li><a href="faq.php">FAQ</a></li>
+                <li><a href="contact.php">contact</a></li>
+                <li><a href="acheter.php">Acheter</a></li>
+            </ul>
+            <div class="container-login-user">
+                <img src="../ressources/image/utilisateur.png" alt="logo-utilisateur">
+            </div>
+        </div>
+    </div>
+    <div class="logo-ico">
+        <img src="../ressources/image/ICO-template-logo.png" alt="logo-ICO">
+    </div>
+</div>
+</body>
