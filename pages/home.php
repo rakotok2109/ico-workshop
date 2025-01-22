e2a8d3bfa516f6ce294995c6f478b25a2ca99390
alvyn <alvyn.richard@hotmail.fr>
2025-01-22 09:20:25 +0100

    Page home pratiquement terminé by Alvyn

--- pages/home.php
+++ pages/home.php
@@ -30,5 +30,37 @@ require 'components/headerHero.php';
     <span class="sr-only">Next</span>
   </a>
 </div>
-</body>
-</html>
+
+<div class="histoire-du-jeu">
+    <h2>Synopsie</h2>
+    <span>
+        <p>Perdus dans la mer des Caraïbes, nos explorateurs n’ont plus qu’une seule solution afin de rentrer chez eux. Ils doivent récolter les 10 trésors de Barbe Rousse situés sur l’île interdite!!! (woouuuh on a peur)
+        Pour mener à bien leur mission, les marins devront démasquer les pirates au sein de leur équipage. Le but des pirates? Empoisonner les marins afin de faire échouer les expéditions vers l’île interdite. Mais attention, des bonus viendront pimenter les débats et compliqueront la tâche des pirates. Serez- vous prêts à braver marées et tempêtes pour vaincre les pirates? Ou bien trahirez-vous vos amis en faisant échouer les marins?</p>
+    </span>
+</div>
+
+<div class="presentation-fondateurs">
+    <div class="presentation-fondateur ">
+        <div class="container-gouvernail ">
+            <img src="../ressources/image/gouvernail.png" alt="gouvernail">
+            <div class="photo-fondateur1">
+                <img src="../ressources/image/chinoise.jpg"alt="photo du fondateur 1">
+            </div>
+        </div>
+        <div class="text-presentation">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime error totam cumque possimus quis magni obcaecati culpa minima nostrum vero vel, vitae mollitia omnis eos! Veniam, excepturi eaque voluptates, soluta, eveniet veritatis saepe provident ratione deserunt nihil quasi minima neque?</p>
+        </div>
+        
+    </div>
+    <div class="presentation-fondateur">
+        <div class="text-presentation">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum beatae, repellat ab officia omnis consequuntur repellendus consequatur aspernatur iste sed! Corporis rem molestias qui hic. Velit, repellendus et, modi fuga voluptas quibusdam autem illo est nobis ad harum doloribus sint?</p>
+        </div>
+        <div class="container-gouvernail">
+            <img src="../ressources/image/gouvernail.png" alt="gouvernail">
+            <div class="photo-fondateur">
+                <img src="../ressources/image/chinoise.jpg" alt="photo du foncateur 2">
+            </div>
+        </div>
+    </div>
+</div>
