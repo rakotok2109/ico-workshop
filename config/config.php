9760c9dd713609d34238b7739807d26d56d087db
Charbel SONON <charbelsnn@gmail.com>
2025-01-23 09:35:04 +0100

    update

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'development' => [
+        'host' => 'localhost',
+        'dbname' => 'workshop-ico',
+        'username' => 'root',
+        'password' => '',
+    ],
+    'production' => [
+        'host' => 'localhost',
+        'dbname' => 'u611341706_ico_workshop',
+        'username' => 'u611341706_ico',
+        'password' => 'Asking@1234',
+    ],
+]; 
--- config/controller/PDOUtils.php
+++ config/controller/PDOUtils.php
@@ -5,54 +5,60 @@ class PDOUtils {
     private static $sharedInstance_;
 
     private function __construct() {
-
-        $this->pdo_ = new PDO('mysql:host=localhost;dbname=workshop-ico', 'root', '', [  PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',]); }
-
-
-    
-        public static function getSharedInstance()
-        {
-            if (!isset(PDOUtils::$sharedInstance_)) {
-                PDOUtils::$sharedInstance_ = new PDOUtils();
-            }
+        $config = require(dirname(__DIR__) . '/config/config');
         
-            return PDOUtils::$sharedInstance_;
-        }
+        // Définir l'environnement ici (par exemple, 'development' ou 'production')
+        $environment = 'production'; 
+        // $environment = 'development'; 
+        $dbConfig = $config[$environment];
 
-        public function requestSQL($sql, $params = null) {
-            $statement = $this->pdo_->prepare($sql);
-            
-            // Vérifie si la préparation de la requête a réussi
-            if ($statement) {
-                // Exécute la requête avec les paramètres fournis
-                if ($statement->execute($params)) {
-                   
-                    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-                    unset($statement);
-                    return $result;
-                } else {
-                    // Gérer les erreurs d'exécution
-                    throw new Exception('Erreur lors de l\'exécution de la requête : ' . implode(", ", $statement->errorInfo()));
-                }
-            } else {
-                // Gérer les erreurs de préparation
-                throw new Exception('Erreur lors de la préparation de la requête : ' . implode(", ", $this->pdo_->errorInfo()));
-            }
-        }
-        public function execSQL($sql, $params = null) {
-            $statement = $this->pdo_->prepare($sql);
-            if($statement && $statement->execute($params)) {
-               
-                unset($statement);
-                return true;
-            }
-            return false;
-           
-        }
+        $dsn = "mysql:host={$dbConfig['host']};dbname={$dbConfig['dbname']};charset=utf8";
+        $this->pdo_ = new PDO($dsn, $dbConfig['username'], $dbConfig['password'], [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        ]);
+    }
 
-        public function lastInsertId() {
-            return $this->pdo_->lastInsertId();
+    public static function getSharedInstance()
+    {
+        if (!isset(PDOUtils::$sharedInstance_)) {
+            PDOUtils::$sharedInstance_ = new PDOUtils();
         }
     
-      
+        return PDOUtils::$sharedInstance_;
+    }
+
+    public function requestSQL($sql, $params = null) {
+        $statement = $this->pdo_->prepare($sql);
+        
+        // Vérifie si la préparation de la requête a réussi
+        if ($statement) {
+            // Exécute la requête avec les paramètres fournis
+            if ($statement->execute($params)) {
+               
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+                unset($statement);
+                return $result;
+            } else {
+                // Gérer les erreurs d'exécution
+                throw new Exception('Erreur lors de l\'exécution de la requête : ' . implode(", ", $statement->errorInfo()));
+            }
+        } else {
+            // Gérer les erreurs de préparation
+            throw new Exception('Erreur lors de la préparation de la requête : ' . implode(", ", $this->pdo_->errorInfo()));
+        }
+    }
+    public function execSQL($sql, $params = null) {
+        $statement = $this->pdo_->prepare($sql);
+        if($statement && $statement->execute($params)) {
+           
+            unset($statement);
+            return true;
+        }
+        return false;
+       
+    }
+
+    public function lastInsertId() {
+        return $this->pdo_->lastInsertId();
+    }
 }
