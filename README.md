4e359caafe2748edc6377ebe51a6551e6e3c78d4
Charbel SONON <charbelsnn@gmail.com>
2025-01-24 02:05:45 +0100

    reset main

--- README.md
+++
@@ -1,2 +0,0 @@
-# ico-workshop
- Site Web - Projet Workshop B3 sur la marque de Jeu de Société ICO
--- ressources/js/dynamiqueCardDashboard.js
+++
@@ -1,9 +0,0 @@
-function switchTab(tabName) {
-    // Désactiver tous les onglets et cacher leurs contenus
-    document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
-    document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
-
-    // Activer l'onglet sélectionné et afficher son contenu
-    document.getElementById(tabName + '-tab').classList.add('active');
-    document.getElementById(tabName + '-content').classList.add('active');
-}
